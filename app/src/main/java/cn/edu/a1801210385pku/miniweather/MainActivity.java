cf460344ed4e6942695a833aee4c686d065018ca
Wangtao <592159596@qq.com>
2018-09-25 22:07:20 +0800

    init project

---
+++ app/src/main/java/cn/edu/a1801210385pku/miniweather/MainActivity.java
@@ -0,0 +1,13 @@
+package cn.edu.a1801210385pku.miniweather;
+
+/**
+ * Created by 涛 on 2018/9/25.
+ */
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.weather_info);
+    }
+}

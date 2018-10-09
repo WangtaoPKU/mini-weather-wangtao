a1628b09fe2a68c230a3d630d7fea932d5048bc0
Wangtao <592159596@qq.com>
2018-10-09 11:57:13 +0800

    检查网络

--- app/src/main/java/cn/edu/a1801210385pku/miniweather/MainActivity.java
+++ app/src/main/java/cn/edu/a1801210385pku/miniweather/MainActivity.java
@@ -2,6 +2,10 @@ package cn.edu.a1801210385pku.miniweather;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import cn.edu.pku.wangtao.util.NetUtil;
 
 /**
  * Created by 涛 on 2018/9/25.
@@ -12,5 +16,13 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.weather_info);
+        if (NetUtil.getNetworkState(this)!=NetUtil.NETWORN_NONE){
+            Log.d("myWeather","网络挂了");
+            Toast.makeText(MainActivity.this,"网络OK!",Toast.LENGTH_LONG).show();
+        }else
+        {
+            Log.d("myWeather","网络挂了");
+            Toast.makeText(MainActivity.this,"网络挂了!",Toast.LENGTH_LONG).show();
+        }
     }
 }
